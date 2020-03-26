ef069d43c3b4c330e7fd2c801a6637872050d77a
Enej <enej@Enejs-MacBook-Pro.local>
2020-03-26 21:58:18 +0100

    customising the message cells

---
+++ eChat-iOS13/Views/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  eChat-iOS13
+//
+//  Created by Enej on 26/03/2020.
+//  Copyright © 2020 Enej. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
