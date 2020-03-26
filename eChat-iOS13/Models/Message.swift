5a648ab448e00f252c5d413c4076282404306291
Enej <enej@Enejs-MacBook-Pro.local>
2020-03-26 21:28:23 +0100

    set up the UITableView

---
+++ eChat-iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  MEssage.swift
+//  eChat-iOS13
+//
+//  Created by Enej on 26/03/2020.
+//  Copyright © 2020 Enej. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
